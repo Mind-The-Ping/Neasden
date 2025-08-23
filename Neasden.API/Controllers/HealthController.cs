18aea1e538996c5f074ba022975677f873e6c266
Darren <darren.daley@hotmail.co.uk>
2025-08-23 17:01:40 +0100

    added a health endpoint

---
+++ Neasden.API/Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Neasden.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("API is live!");
+}
