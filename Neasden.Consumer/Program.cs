5b3ce0db66271ca157a4bb4a22e340fb47172011
Darren <darren.daley@hotmail.co.uk>
2025-09-10 09:04:00 +0100

    update complete

--- Neasden.Consumer/Program.cs
+++ Neasden.Consumer/Program.cs
@@ -1,4 +1,27 @@
-using Neasden.Consumer;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Neasden.Consumer.Repositories;
+using Neasden.Repository.Database;
+using Neasden.Repository.Repositories;
 
-var host = HostFactory.CreateHost(args);
-host.Run();
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Services.AddDbContext<NeasdenDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetSection("PostgreSQL")["ConnectionString"]));
+
+builder.Services.AddScoped<DisruptionRepository>();
+builder.Services.AddScoped<NotificationRepository>();
+
+builder.Services.AddScoped<DisruptionConsumerRepo>();
+builder.Services.AddScoped<NotificationConsumerRepo>();
+
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+builder.Build().Run();
