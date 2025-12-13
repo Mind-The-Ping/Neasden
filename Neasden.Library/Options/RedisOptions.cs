b2b09db8150c5760a21fb25210276253b98a8689
Kinaetron <darren.daley@hotmail.co.uk>
2025-12-13 10:57:04 +0000

    weezo

--- Neasden.Library/Options/RedisOptions.cs
+++ Neasden.Library/Options/RedisOptions.cs
@@ -2,4 +2,7 @@
 public class RedisOptions
 {
     public required string Connection { get; set; }
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
+    public int Port { get; set; }
 }
